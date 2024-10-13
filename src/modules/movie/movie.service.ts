2fa7b7bd492c31649253fe4e32a9eb79de6bde0e
bilalsohailmodi1 <38935971+BilalSohailModi@users.noreply.github.com>
2024-10-13 12:49:18 +0500

    fix: get movies query fix

--- src/modules/movie/movie.service.ts
+++ src/modules/movie/movie.service.ts
@@ -13,40 +13,38 @@ export class MovieService {
   constructor(
     @InjectRepository(MovieEntity)
     private movieRepository: Repository<MovieEntity>,
-  ) {
-
-  }
+  ) {}
   async create(createMovieDto: CreateMovieDto, createdBy: UserEntity) {
-    const movie = this.movieRepository.create({ ...createMovieDto, createdBy })
+    const movie = this.movieRepository.create({ ...createMovieDto, createdBy });
     await this.movieRepository.save(movie).catch((err) => {
-      throw new CustomHttpException(err.message, HttpStatus.BAD_REQUEST)
-    })
-    return movie
+      throw new CustomHttpException(err.message, HttpStatus.BAD_REQUEST);
+    });
+    return movie;
   }
 
   async findAll({ offset, limit, getAll }: iPagintionQuery) {
-    const fetchAll = getAll == 'true'
+    const fetchAll = getAll == 'true';
     const options: FindManyOptions<MovieEntity> = {
       where: { deletedAt: null },
       order: { createdAt: 'DESC' }, // Use 'DESC' for descending order
       skip: offset,
       take: limit,
-    }
+    };
     if (fetchAll) {
-      delete options.skip
-      delete options.take
+      delete options.skip;
+      delete options.take;
     }
-    const [movie, total] = await this.movieRepository.findAndCount();
+    const [movie, total] = await this.movieRepository.findAndCount(options);
     return getPaginated({
       data: movie,
       offset: fetchAll ? -1 : offset,
       limit: fetchAll ? -1 : limit,
-      total
-    })
+      total,
+    });
   }
 
   async update(id: string, updateMovieDto: UpdateMovieDto) {
-    const result = await this.movieRepository.update(id, updateMovieDto)
-    return result
+    const result = await this.movieRepository.update(id, updateMovieDto);
+    return result;
   }
 }
